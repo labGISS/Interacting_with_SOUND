ea6e72eedf515ac8993b39c08c27744e5509795b
vincios <vinciosdev@gmail.com>
2023-12-05 15:03:20 +0100

    Fix static file collection error and chang

--- prin_site/settings.py
+++ prin_site/settings.py
@@ -163,6 +163,6 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": 'whitenoise.storage.CompressedStaticFilesStorage',
+        "BACKEND": 'whitenoise.storage.CompressedManifestStaticFilesStorage',
     },
 }
