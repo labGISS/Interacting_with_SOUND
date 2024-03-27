53430cccacc426e6c1d624440afb541888fd03b5
Vincenzo <vinciosdev@gmail.com>
2024-03-27 14:43:01 +0100

    prin: add app namespace

--- prin/urls.py
+++ prin/urls.py
@@ -1,6 +1,7 @@
 from prin.views import *
 from django.urls import path
 
+app_name = 'prin'
 urlpatterns = [
     path('index/', home, name="index"),
     path('analytics/', analytics, name="analytics"),
